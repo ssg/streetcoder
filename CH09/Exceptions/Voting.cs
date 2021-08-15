ca43a89e07f7501162eba393a49876e9e944f756
Sedat Kapanoglu <sedat@kapanoglu.com>
2021-08-14 17:13:25 -0700

    fix spacing

--- CH09/Exceptions/Voting.cs
+++ CH09/Exceptions/Voting.cs
@@ -11,18 +11,18 @@ namespace Exceptions {
   public class Voting : Controller {
     private readonly IDatabase db;
 
-[HttpPost]
-public IActionResult Upvote(Guid contentId) {
-  var result = db.Upvote(contentId);
-  return result switch {
-    VotingResult.Success 
-      => success(),
-    VotingResult.ContentTooOld 
-      => warning("Content is too old. It can't be voted"),
-    VotingResult.ContentDeleted 
-      => warning("Content is deleted. It can't be voted"),
-  };
-}
+    [HttpPost]
+    public IActionResult Upvote(Guid contentId) {
+      var result = db.Upvote(contentId);
+      return result switch {
+        VotingResult.Success
+          => success(),
+        VotingResult.ContentTooOld
+          => warning("Content is too old. It can't be voted"),
+        VotingResult.ContentDeleted
+          => warning("Content is deleted. It can't be voted"),
+      };
+    }
 
     private IActionResult success() {
       throw new NotImplementedException();
