7df98c4474bf5573e9a20730a9b8f87800856339
Sedat Kapanoğlu <sedat@kapanoglu.com>
2022-08-30 20:16:42 -0700

    fix missing early return in MultiplyEachSIMD

--- CH07/Patterns/BufferProcessor.cs
+++ CH07/Patterns/BufferProcessor.cs
@@ -2,24 +2,25 @@
 
 namespace Patterns {
   public class BufferProcessor {
-public static void MultiplyEachSIMD(int[] buffer, int value) {
-  if (!Vector.IsHardwareAccelerated) {
-    MultiplyEachClassic(buffer, value);
-  }
+    public static void MultiplyEachSIMD(int[] buffer, int value) {
+      if (!Vector.IsHardwareAccelerated) {
+        MultiplyEachClassic(buffer, value);
+        return;
+      }
 
-  int chunkSize = Vector<int>.Count;
-  int n = 0;
-  for (; n < buffer.Length - chunkSize; n += chunkSize) {
-    var vector = new Vector<int>(buffer, n);
-    vector *= value; // multiply all values in the vector at once
-    vector.CopyTo(buffer, n); // replace the values
-  }
+      int chunkSize = Vector<int>.Count;
+      int n = 0;
+      for (; n < buffer.Length - chunkSize; n += chunkSize) {
+        var vector = new Vector<int>(buffer, n);
+        vector *= value; // multiply all values in the vector at once
+        vector.CopyTo(buffer, n); // replace the values
+      }
 
-  // process remaining bytes
-  for (; n < buffer.Length; n++) {
-    buffer[n] *= value;
-  }
-}
+      // process remaining bytes
+      for (; n < buffer.Length; n++) {
+        buffer[n] *= value;
+      }
+    }
 
     public static void MultiplyEachClassic(int[] buffer, int value) {
       for (int n = 0; n < buffer.Length; n++) {
