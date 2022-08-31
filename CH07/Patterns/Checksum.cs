78c6a9c84a7ac6fa98e0f78212194fa0ddf2a820
Sedat Kapanoğlu <sedat@kapanoglu.com>
2022-08-30 20:00:41 -0700

    fix the remainingSum calculation error

--- CH07/Patterns/Checksum.cs
+++ CH07/Patterns/Checksum.cs
@@ -10,21 +10,21 @@ namespace Patterns {
       return result;
     }
 
-public static int CalculateChecksumParallel(byte[] array) {
-  int r0 = 0, r1 = 0, r2 = 0, r3 = 0;
-  int len = array.Length;
-  int i = 0;
-  for (; i < len - 4; i += 4) {
-    r0 += array[i + 0];
-    r1 += array[i + 1];
-    r2 += array[i + 2];
-    r3 += array[i + 3];
-  }
-  int remainingSum = 0;
-  for (; i < len; i++) {
-    remainingSum += i;
-  }
-  return r0 + r1 + r2 + r3 + remainingSum;
-}
+    public static int CalculateChecksumParallel(byte[] array) {
+      int r0 = 0, r1 = 0, r2 = 0, r3 = 0;
+      int len = array.Length;
+      int i = 0;
+      for (; i < len - 4; i += 4) {
+        r0 += array[i + 0];
+        r1 += array[i + 1];
+        r2 += array[i + 2];
+        r3 += array[i + 3];
+      }
+      int remainingSum = 0;
+      for (; i < len; i++) {
+        remainingSum += array[i];
+      }
+      return r0 + r1 + r2 + r3 + remainingSum;
+    }
   }
 }
